57fd704d30e413cadcd32b7101654e5634959372
niladhikari <niladhikari445@gmail.com>
2023-12-22 13:42:15 +0600

    add the put and delete method and get method using the id ways for the update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5020;
 
 // Middleware
 app.use(cors());
@@ -28,6 +28,7 @@ async function run() {
 
     const userCollection = client.db("taskManagementDb").collection("users");
     const reviewsCollection = client.db("taskManagementDb").collection("reviews");
+    const taskCollection = client.db("taskManagementDb").collection("tasks");
 
     //post method  add the user in the database
     // insert email if user does not exists:
@@ -42,7 +43,59 @@ async function run() {
       res.send(result);
     });
 
-    //get method  add the user in the database
+    //for the add the tasks in the database
+    app.post("/tasks", async (req, res) => {
+      const taskItem = req.body;
+      const result = await taskCollection.insertOne(taskItem);
+      res.send(result);
+    });
+    //get method  add the tasks in the database
+    app.get("/tasks", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
+   //get method the tasks email ways in the database
+    app.get("/task/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //get operation for update the task data pass the db
+    app.get("/myTask/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    });
+
+      // delete method for the delete the task in the db
+      app.delete("/tasks/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
+      });
+
+      //patch operation for update the task  and pass the database
+    app.put("/tasks/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          title: item.title,
+          priority: item.priority,
+          deadlines: item.deadlines,
+          descriptions: item.descriptions,
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    //get method  add the reviews in the database
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
